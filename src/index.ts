9dc0e0871b1c0e58a1eec929fefaadb704534838
Trung Nguyen <ntrung03@outlook.com>
2020-12-15 11:47:47 +0700

    Proper http server response

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { config } from 'dotenv';
 config();
-import { createServer } from 'https';
-createServer().listen(process.env.PORT || 8080);
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+createServer((req: IncomingMessage, res: ServerResponse) => {
+    res.writeHead(200);
+    res.end('Hello world!\n');
+}).listen(process.env.PORT || 8080);
 import { InariClient } from './struct/bot/Client';
 const client = new InariClient();
 client.start();
