11825190d60bb120e30f9f73eb3b268b8c57e0c2
t41y0u <vuthaiduong0607@gmail.com>
2021-01-03 14:28:05 +0700

    Modify wipe script

--- scripts/wipe.ts
+++ scripts/wipe.ts
@@ -1,10 +1,10 @@
 import { config } from 'dotenv';
 config();
-import { init } from '../src/struct/db';
+import { init } from '../src/database';
 init();
 import { disconnect } from 'mongoose';
-import { User } from '../src/struct/db/models/user';
-import { Server } from '../src/struct/db/models/server';
+import { User } from '../src/database/models/user';
+import { Server } from '../src/database/models/server';
 
 (async () => {
     disconnect();
