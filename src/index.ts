90c4a130af70d08689e0dc76728bc9ea8f754d32
t41y0u <vuthaiduong0607@gmail.com>
2021-04-17 15:03:37 +0700

    Fix undefined npm_package_version bug

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { config } from 'dotenv';
 config();
 import { createServer } from 'http';
 createServer().listen(process.env.PORT || 8080);
+import path from 'path';
+path.join(__dirname, 'package.json');
 import { Client } from './structures/Client';
 const client = new Client();
 client.start();
