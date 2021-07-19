cf180c58ba26b3b9d3c5cad5109fc9e4ef98e345
t41y0u <vuthaiduong0607@gmail.com>
2021-07-19 18:12:51 +0700

    Add QNA to help embeds

--- src/commands/info/help.ts
+++ src/commands/info/help.ts
@@ -10,6 +10,7 @@ import {
 import { SUPPORT_SERVER } from '@constants';
 
 const CATEGORIES = {
+    qna: ['❔', 'QNA'],
     asmr: ['👂', 'ASMR'],
     general: ['🧻', 'General'],
     images: ['🖼️', 'Images'],
@@ -43,6 +44,53 @@ export default class extends Command {
 
     async exec(interaction: CommandInteraction) {
         const embeds = new Collection<string, MessageEmbed>();
+        const qna = this.client.embeds
+            .default()
+            .setTitle('❔\u2000Questions Nobody Asked')
+            .setDescription(
+                `If you still have questions, [join the support server](${SUPPORT_SERVER}) and ask at #help`
+            )
+            .addField('Where are the n! commands?', 'Gone. Completely migrated to slash commands.')
+            .addField(
+                'How to view other pages or sort by popularity?',
+                'After typing the query, press TAB once to input page number, press TAB one more time to input sort method. You can delete the page prompt if you only need to check out the first page. Page number is `1` and sort method is `recent` by default.'
+            )
+            .addField(
+                'What do these buttons/select menus do?',
+                '• `<<` / `>>` : Jumps to first/last page\n' +
+                    '• `<` / `>` : Jumps to previous/next page⁽¹⁾\n' +
+                    '• `x of y` : Jumps to specified page⁽¹⁾\n' +
+                    '• `Sauce?` : Searches for image source using SauceNAO\n' +
+                    '• `❤️` : Adds/Removes a doujin to/from favorites\n' +
+                    '• `🔖` : Follows/Unfollows a tag/artist/parody/etc.\n' +
+                    '• `🏴` : Blacklists a tag/artist/parody/etc.\n' +
+                    '• `📥` : Downloads current doujin\n' +
+                    "• `🗑` : Deletes bot message (and sometimes the user's message)⁽¹⁾\n" +
+                    '• `Info View` / `Thumbnail View` / `Preview`: Toggles between text mode (with tags, artists, etc.)/big images mode/start reading the doujin. For `g` and `random` commands, `Thumbnail View` actually means start reading.⁽¹⁾\n' +
+                    '(1) **Only the person who used the command can use these buttons/select menus**'
+            )
+            .addField(
+                'What does following a tag/artist/parody/etc. mean?',
+                "It's a feature that notifies you through DM when a new doujin with a tag you followed was released. You have to allow DM for it to work (obviously)."
+            )
+            .addField(
+                'Why sometimes images are not showing?',
+                'There are many possible reasons:\n' +
+                    '• The media you are viewing contains a banned tag. The bot decided to omit the images to protect the server and itself. You can still unlock them by using the `danger` command. The bot owner will not take any responsibilities if this caused your server to get banned.\nRead [Discord Community Guidelines](https://discord.com/guidelines) for more info. TL;DR: loli, shota, guro.\n' +
+                    '• You blacklisted one of the tags.\n' +
+                    '• Discord AI deems this media unfit to display on Discord.\n' +
+                    '• Image link is dead.\n' +
+                    '• Your internet sucks.\n' +
+                    'Note: It is obviously impossible to `Preview` a doujin with banned tags.'
+            )
+            .addField(
+                'Why sometimes the bot just stopped working?',
+                'Again, there are many possible reasons:\n' +
+                    "• If it's only the buttons that didn't work, it could be that no buttons were clicked in that message for more than 3-5 minutes, so the bot just stopped listening.\n" +
+                    '• The bot is hosted on Heroku, which restarts roughly every 24-hour.\n' +
+                    '• A new update just came out and the bot needed to restart to apply new changes.'
+            );
+        embeds.set('qna', qna);
         for (const [category, commandNames] of this.client.categories.entries()) {
             if (category === 'owner' && interaction.user.id !== this.client.ownerID) continue;
             const commands = commandNames.map(c => this.client.commands.get(c).data);
@@ -50,13 +98,16 @@ export default class extends Command {
                 .default()
                 .setTitle(CATEGORIES[category].join('\u2000'))
                 .setDescription(
-                    'Note: All commands are slash commands, a feature Discord [introduced](https://blog.discord.com/slash-commands-are-here-8db0a385d9e6) not long ago.'
+                    'Note: All commands are slash commands, a feature Discord [introduced](https://blog.discord.com/slash-commands-are-here-8db0a385d9e6) not long ago. Commands with the `🔞` icon are NSFW commands and can only be used in NSFW channels.'
                 )
                 .addField(
                     'Commands',
                     commands.map(c => `${c.nsfw ? '`🔞`' : ''}__\`${c.name}\`__`).join(' ')
                 )
-                .addField('Confused?', `[Join the support server](${SUPPORT_SERVER})`);
+                .addField(
+                    'Confused?',
+                    `Check out the QNA page!\nIf you still have questions, [join the support server](${SUPPORT_SERVER})`
+                );
             embeds.set(category, embed);
         }
         const message = (await interaction.editReply({
