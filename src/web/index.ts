f2b365622deb4b78c08a5dc3deb14d45c2e9b33a
Thai Duong Vu <vuthaiduong0607@gmail.com>
2021-12-20 17:11:51 +0700

    Better error catching for web server

--- src/web/index.ts
+++ src/web/index.ts
@@ -18,51 +18,51 @@ app.get('/download/:code/', async (req, res) => {
     let gallery: Gallery = null;
     try {
         gallery = (await api.g(+code)).gallery;
+        if (!gallery || !gallery.tags) return res.redirect('/');
+        res.writeHead(200, {
+            'Content-Type': 'application/pdf',
+            'Access-Control-Allow-Origin': '*',
+            'Content-Disposition': `attachment; filename=${code}.pdf`,
+        });
+        const {
+            title: { english: title },
+            upload_date,
+            tags,
+            images: { pages: images },
+        } = gallery;
+        const t = new Map();
+        tags.sort((a, b) => b.count - a.count);
+        tags.forEach(tag => {
+            const { type, name } = tag;
+            const a = t.get(type) || [];
+            a.push(name);
+            t.set(type, a);
+        });
+        const doc = new PDFDocument({
+            margin: 50,
+            info: {
+                Title: he.decode(title),
+                Author: t.get('artist').join(', '),
+                Keywords: t.get('tag').join(', '),
+                CreationDate: new Date(upload_date * 1000),
+            },
+            autoFirstPage: false,
+        });
+        const stream = doc.pipe(res);
+        const pages = api.getPages(gallery);
+        for await (const [idx, page] of pages.entries()) {
+            const { w, h } = images[idx];
+            doc.addPage({ size: [w, h] });
+            const data = await axios
+                .get(page, { responseType: 'arraybuffer' })
+                .then(res => Buffer.from(res.data));
+            doc.image(data, 0, 0);
+        }
+        doc.end();
+        stream.on('end', () => res.end());
     } catch (err) {
         if (err) return res.redirect('/');
     }
-    if (!gallery || !gallery.tags) return res.redirect('/');
-    res.writeHead(200, {
-        'Content-Type': 'application/pdf',
-        'Access-Control-Allow-Origin': '*',
-        'Content-Disposition': `attachment; filename=${code}.pdf`,
-    });
-    const {
-        title: { english: title },
-        upload_date,
-        tags,
-        images: { pages: images },
-    } = gallery;
-    const t = new Map();
-    tags.sort((a, b) => b.count - a.count);
-    tags.forEach(tag => {
-        const { type, name } = tag;
-        const a = t.get(type) || [];
-        a.push(name);
-        t.set(type, a);
-    });
-    const doc = new PDFDocument({
-        margin: 50,
-        info: {
-            Title: he.decode(title),
-            Author: t.get('artist').join(', '),
-            Keywords: t.get('tag').join(', '),
-            CreationDate: new Date(upload_date * 1000),
-        },
-        autoFirstPage: false,
-    });
-    const stream = doc.pipe(res);
-    const pages = api.getPages(gallery);
-    for await (const [idx, page] of pages.entries()) {
-        const { w, h } = images[idx];
-        doc.addPage({ size: [w, h] });
-        const data = await axios
-            .get(page, { responseType: 'arraybuffer' })
-            .then(res => Buffer.from(res.data));
-        doc.image(data, 0, 0);
-    }
-    doc.end();
-    stream.on('end', () => res.end());
 });
 
 app.get('/', (req, res) => {
@@ -76,3 +76,6 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
     log.info(`[WEB] Listening at port ${port}.`);
 });
+
+process.on('uncaughtException', err => log.stackTrace(err));
+process.on('unhandledRejection', err => log.stackTrace(err));
