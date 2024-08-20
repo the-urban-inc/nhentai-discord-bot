2b91bdb426129cb3e4bcd51c11723e1113768ced
t41y0u <vuthaiduong0607@gmail.com>
2024-08-20 15:42:19 +1000

    Remove private URL

--- src/api/nhentai/index.ts
+++ src/api/nhentai/index.ts
@@ -33,7 +33,7 @@ export interface TagResult {
 }
 
 export class Client {
-    public baseURL = process.env.NHENTAI_URL || 'https://nhentai.net';
+    public baseURL = 'https://nhentai.net';
     public baseImageURL = 'https://i.nhentai.net';
     public baseThumbnailURL = 'https://t.nhentai.net';
 
